7c36656654e3ac1ec40feae2a7f43832a917d616
Davis W Frank & Tim Huertas <pair+dwfrank+huertas@projectchelsea.com>
2012-03-05 16:05:25 -0800

    adding ssh_port to deploy cap task

--- lib/generators/lobot/templates/deploy-ci.rb
+++ lib/generators/lobot/templates/deploy-ci.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 aws_conf_location = File.expand_path('../../ci.yml', __FILE__)
-ci_server = YAML.load_file(aws_conf_location)["server"]['elastic_ip']
+server_config = YAML.load_file(aws_conf_location)["server"]
+ci_server = server_config['elastic_ip']
+ssh_port = server_config['ssh_port'] || 22
 
-role :ci, ci_server
+role :ci, "#{ci_server}:#{ssh_port}"
