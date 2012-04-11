11bef9b4c3e5bf1cd8ffe957a84449469cb4c0e0
Michael Schubert & Trace Wax <pair+mschubert+trace@pivotallabs.com>
2012-04-11 11:23:57 -0400

    Standard usage of rvm-capistrano
    (See http://beginrescueend.com/integration/capistrano/)

--- lib/generators/lobot/templates/capistrano-ci.rb
+++ lib/generators/lobot/templates/capistrano-ci.rb
@@ -1,13 +1,15 @@
 require 'capistrano/ext/multistage'
-require "rvm/capistrano"  # Use the gem, don't unshift RVM onto the load path, that's crazy.
 
 task :ci_setup do
   ci_conf_location ||= File.expand_path('../../../config/ci.yml', __FILE__)
   ci_conf ||= YAML.load_file(ci_conf_location)
 
-  raise "rvm_ruby_string not set - are you using RVM" unless ENV['rvm_ruby_string']
-  set :rvm_ruby_string, ENV['rvm_ruby_string']
-  set :rvm_type, :user
+  if ENV["rvm_path"]
+    require "bundler/capistrano"
+    require "rvm/capistrano"  # Use the gem, don't unshift RVM onto the load path, that's crazy.
+    set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+    set :rvm_type, :user
+  end
   set :app_name, ci_conf['app_name']
   set(:app_dir) { "/var/#{stage}/#{app_name}" }
   set :user, ci_conf['app_user']
