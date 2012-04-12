f87dabb474a51e6e29177e546575693383fd4392
Michael Schubert & Trace Wax <pair+mschubert+trace@pivotallabs.com>
2012-04-12 14:06:07 -0400

    Add to the sample ci_build script copying of a database.yml if one does not exist

--- lib/generators/lobot/templates/ci_build.sh
+++ lib/generators/lobot/templates/ci_build.sh
@@ -11,6 +11,9 @@ echo USER=$USER && ruby --version && which ruby && which bundle
 # conditionally install project gems from Gemfile
 bundle check || bundle install || exit 1
 
+test -e config/database.yml || (test -e config/database.yml.example && cp config/database.yml.example config/database.yml)
+test -e config/database.yml || (test -e config/database.example.yml && cp config/database.example.yml config/database.yml)
+
 RAILS_ENV=development rake db:version > /dev/null || rake db:create
 RAILS_ENV=test rake db:version  > /dev/null || rake db:create
 
