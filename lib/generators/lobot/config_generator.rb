1d5c62ee55494b7df0abb46776b085d170983634
Michael Schubert & Trace Wax <pair+mschubert+trace@pivotallabs.com>
2012-04-13 11:04:26 -0400

    Update lobot:config - add colors, remove questions that most people won't change and make instructions more clear

--- lib/generators/lobot/config_generator.rb
+++ lib/generators/lobot/config_generator.rb
@@ -12,11 +12,11 @@ module Lobot
       default_app_name = File.basename(Rails.root)
       config = {
         'app_name' => default_app_name,
-        'app_user' => ENV['USER'],
+        'app_user' => "ci",
         'git_location' => default_git_location,
         'basic_auth' => [
           {
-            'username' => "#{default_app_name}_ci"
+            'username' => "ci"
           }
         ],
         'credentials' => {
@@ -35,80 +35,65 @@ module Lobot
         "github_private_ssh_key_path" => File.expand_path("~/.ssh/id_rsa")
       }
 
-      say "* The name of your application as it will appear in CI"
-      app_name = ask "Application Name [#{config['app_name']}]: "
+      say "* The name of your application as it will appear in CI", :green
+      app_name = ask("Application Name [#{config['app_name']}]:", :bold)
       config['app_name'] = app_name if app_name != ""
 
-      say "* The user created to run your CI process"
-      app_user = ask "Application User [#{config['app_user']}]: "
-      config['app_user'] = app_user if app_user != ""
-
-      say "* The location of your remote git repository which CI will poll and pull from on changes"
-      git_location = ask "Git Repository Location [#{config['git_location']}]: "
+      say "* The location of your remote git repository which CI will poll and pull from on changes", :green
+      git_location = ask("Git Repository Location [#{config['git_location']}]:", :bold)
       config['git_location'] = git_location if git_location != ""
 
-      say "* The username you will use to access the CI web interface"
-      ci_username = ask "CI Username [#{config['basic_auth'][0]['username']}]: "
+      say "* The username you will use to access the CI web interface", :green
+      ci_username = ask("CI Username [#{config['basic_auth'][0]['username']}]:", :bold)
       config['basic_auth'][0]['username'] = ci_username if ci_username != ""
 
-      say "* The password you will use to access the Jenkins web interface"
+      say "* The password you will use to access the CI web interface", :green
       while true do
-        ci_password = ask "Choose a CI password: "
+        ci_password = ask("Choose a CI password:", :bold)
         config['basic_auth'][0]['password'] = ci_password
         if ci_password == ""
-          say "Password cannot be blank"
+          say "Password cannot be blank", :red
         else
           break
         end
       end
 
-      say <<-EOS
+      say (<<-EOS).chop, :green
+
 * See https://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key
 * for access key id and secret access key
       EOS
-
       while true do
-        aws_access_key_id = ask "AWS Access Key ID: "
+        aws_access_key_id = ask("AWS Access Key ID:", :bold)
         config['credentials']['aws_access_key_id'] = aws_access_key_id
         if aws_access_key_id == ""
-          say "AWS Access Key ID cannot be blank"
+          say "AWS Access Key ID cannot be blank", :red
         else
           break
         end
       end
 
       while true do
-        aws_secret_access_key = ask "AWS Secret Access Key: "
+        aws_secret_access_key = ask("AWS Secret Access Key:", :bold)
         config['credentials']['aws_secret_access_key'] = aws_secret_access_key
         if aws_secret_access_key == ""
-          say "AWS Secret Access Key cannot be blank"
+          say "AWS Secret Access Key cannot be blank", :red
         else
           break
         end
       end
 
-      say <<-EOS
-* See http://aws.amazon.com/ec2/instance-types/ API name values (e.g. m1.large)
-      EOS
-      flavor_id = ask "Choose an EC2 instance type [#{config['server']['flavor_id']}]: "
-      config['server']['flavor_id'] = flavor_id if flavor_id != ""
-
-      security_group = ask "AWS EC2 Security Group Name [#{config['server']['security_group']}]: "
-      config['server']['security_group'] = security_group if security_group != ""
-
-      ssh_port = ask "Server SSH Port [#{config['server']['ssh_port']}]: "
-      config['server']['ssh_port'] = ssh_port if ssh_port != ""
-
-      build_command = ask "Build Command: [#{config['build_command']}]: "
+      build_command = ask("Build Command: [#{config['build_command']}]:", :bold)
       config['build_command'] = build_command if build_command != ""
 
-      say <<-EOS
+      say (<<-EOS).chop, :green
+
 * This should refer to an SSH key pair that you have already generated. You may wish to generate a new key
 * separate from what you may already be using for github or other systems.
 * For a tutorial on this see: http://open.bsdcow.org/histerical/tutorials/ssh_pubkey_auth#1.2
       EOS
       while true do
-        id_rsa_path = ask "Path to SSH Private Key for EC2 Access [#{config['ec2_server_access']['id_rsa_path']}]: "
+        id_rsa_path = ask("Path to SSH Private Key for EC2 Access [#{config['ec2_server_access']['id_rsa_path']}]:", :bold)
         config['ec2_server_access']['id_rsa_path'] = id_rsa_path if id_rsa_path != ""
         if config['ec2_server_access']['id_rsa_path'] != File.expand_path(config['ec2_server_access']['id_rsa_path'])
           config['ec2_server_access']['id_rsa_path'] = File.expand_path(File.join(ENV['HOME'], '.ssh', config['ec2_server_access']['id_rsa_path']))
@@ -116,23 +101,17 @@ module Lobot
         if File.exist?(config['ec2_server_access']['id_rsa_path']) && File.exist?("#{config['ec2_server_access']['id_rsa_path']}.pub")
           break
         else
-          say "Unable to find both #{config['ec2_server_access']['id_rsa_path']} and #{config['ec2_server_access']['id_rsa_path']}.pub"
+          say "Unable to find both #{config['ec2_server_access']['id_rsa_path']} and #{config['ec2_server_access']['id_rsa_path']}.pub", :red
         end
       end
 
-      say <<-EOS
-* This is an arbitrary label corresponding to the SSH credentials that you just selected
-* You may name this anything you like. For example: your project name, hostname or name of the SSH key you just chose
-      EOS
-      key_pair_name = ask "AWS EC2 Key Pair Name [#{config['ec2_server_access']['key_pair_name']}]: "
-      config['ec2_server_access']['key_pair_name'] = key_pair_name if key_pair_name != ""
+      say (<<-EOS).chop, :green
 
-      say <<-EOS
 * This needs to refer to an SSH Private Key that has been associated an account that has access to the git
 * repository you entered above. On github this will be listed here: https://github.com/settings/ssh
       EOS
       while true do
-        github_private_ssh_key_path = ask "Path to SSH Private Key for Github [#{config['github_private_ssh_key_path']}]: "
+        github_private_ssh_key_path = ask("Path to SSH Private Key for Github [#{config['github_private_ssh_key_path']}]:", :bold)
         config['github_private_ssh_key_path'] = github_private_ssh_key_path if github_private_ssh_key_path != ""
         if config['github_private_ssh_key_path'] != File.expand_path(config['github_private_ssh_key_path'])
           config['github_private_ssh_key_path'] = File.expand_path(File.join(ENV['HOME'], '.ssh', config['github_private_ssh_key_path']))
@@ -140,11 +119,9 @@ module Lobot
         if File.exist?(config['github_private_ssh_key_path'])
           break
         else
-          say "Unable to find #{config['github_private_ssh_key_path']}"
+          say "Unable to find #{config['github_private_ssh_key_path']}", :red
         end
       end
-
-
 
       config_ci = YAML.load_file(Rails.root.join("config/ci.yml")) rescue {}
       config_ci.merge!(config)
@@ -153,8 +130,10 @@ module Lobot
         f << config_ci.to_yaml
       end
 
-      say "\n\nconfig/ci.yml configured. To launch your instance run rake ci:server_start."
-      say "Be sure to push uncommitted changes made by the lobot:install process first."
+      say "\n\nconfig/ci.yml configured:\n#{File.read(Rails.root.join('config/ci.yml'))}\n"
+      say "You can edit this file to change any additional defaults."
+      say "Before continuing, be sure to push uncommitted changes to your git repository.", :red
+      say "For next steps, see README.md"
     end
   end
 end
