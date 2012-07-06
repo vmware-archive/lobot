238b419089e387c90ee996e6bc79299d12eda7be
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2012-07-05 18:22:45 -0700

    adding ci_build.sh

---
+++ ci_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+source $HOME/.rvm/scripts/rvm && source .rvmrc
+
+# install bundler if necessary
+gem list --local bundler | grep bundler || gem install bundler || exit 1
+
+# debugging info
+echo USER=$USER && ruby --version && which ruby && which bundle
+
+# conditionally install project gems from Gemfile
+bundle check || bundle install || exit 1
+
+bundle exec rake default --trace
