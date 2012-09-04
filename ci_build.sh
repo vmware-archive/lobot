0a0c737ecf49818ecd1cb66d0bd74e9c66e87e8b
Matthew Kocher <kocher@gmail.com>
2012-09-03 17:34:30 -0700

    removing knownhosts on CI
    Occasionally you'll get the same IP address for a new instance.

--- ci_build.sh
+++ ci_build.sh
@@ -11,6 +11,10 @@ echo USER=$USER && ruby --version && which ruby && which bundle
 # conditionally install project gems from Gemfile
 bundle check || bundle install || exit 1
 
+# remove known_hosts2 - ec2 recycles ips
+# known_hosts is owned by root and trusts github
+rm ~/.ssh/known_hosts2
+
 cp ~/secrets.yml features/config/
 
 bundle exec rake default --trace
